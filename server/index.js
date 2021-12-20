ad2a0c679efcad2239ef7a2df9097c7e70123bde
xiongjieyi <jinjingcs@gmail.com>
2021-12-20 22:13:09 +0100

    receive data at UI

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const axios = require('axios')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3001
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const db = mysql.createPool({
   host: 'localhost',
@@ -14,7 +21,6 @@ const db = mysql.createPool({
 const getRatesfromFixer = async (day) => {
   try {
     return await axios.get(`http://data.fixer.io/api/2021-11-${day}?access_key=602925509d64f5fecce3058df69fe6ab&symbols=AUD,BRL,CNY,GBP,USD`)
-    //console.log(resp.data)
   } catch (err) {
     console.error(err)
   }
@@ -22,58 +28,77 @@ const getRatesfromFixer = async (day) => {
 
 const getCountriesfromLayer = async () => {
   try {
-    return await axios.get('http://api.countrylayer.com/v2/all?access_key=22b078bafdfca6a157d08096e74ecbb8')
     //quert all countries info
+    return await axios.get('http://api.countrylayer.com/v2/all?access_key=22b078bafdfca6a157d08096e74ecbb8')
   } catch (err) {
     console.error(err)
   }
 }
 
-const main = async () => {
-  ////Query data from FIXER
-  // let day = 1
-  // for (day = 1; day <= 30; day++) {
-  //   let resp = []
-  //   if (day <= 9) {
-  //     resp = await getRatesfromFixer("0" + day)
-  //   } else {
-  //     resp = await getRatesfromFixer(day)
-  //   }
-  //   console.log(resp.data)
-  //   const result = resp.data
-  //   const sqlInsert = 'INSERT INTO eurbase_table (date, AUD, BRL, CNY, GBP, USD) VALUES (?, ?, ?, ?, ?, ?);'
-  //   db.query(sqlInsert, [result.date, result.rates.AUD, result.rates.BRL, result.rates.CNY, result.rates.GBP, result.rates.USD], (err, result) => {
-  //     console.log(err)
-  //   })
-  // }
-
-  //Query data from COUNTRYLAYER
-  const resp = await getCountriesfromLayer()
-  const countries = resp.data.filter(country => (
-    country.name === 'Australia' ||
-    country.name === 'Brazil' ||
-    country.name === 'China' ||
-    country.name === 'United Kingdom of Great Britain and Northern Ireland' ||
-    country.name === 'United States of America'
-  ))
-  console.log(countries)
-
-  for (let i = 0; i < countries.length; i++) {
-    const sqlInsert = 'INSERT INTO country_info (country_name, country_code, calling_code, capital, region) VALUES (?, ?, ?, ?, ?);'
-    db.query(sqlInsert, [countries[i].name, countries[i].alpha3Code, countries[i].callingCodes[0], countries[i].capital, countries[i].region], (err, result) => {
-      console.log(err)
-    })
-  }
-
-}
-
-
-
 app.get('/', (req, res) => {
+  res.send('Hello world')
+})
 
-  main()
-  res.send("item is inserted.")
+//Save data to country_info table
+app.get('/api/countries', (req, res) => {
+  const saveCountriesToDB = async () => {
+    const resp = await getCountriesfromLayer()
+    const countries = resp.data.filter(country => (
+      country.name === 'Australia' ||
+      country.name === 'Brazil' ||
+      country.name === 'China' ||
+      country.name === 'United Kingdom of Great Britain and Northern Ireland' ||
+      country.name === 'United States of America'
+    ))
+    console.log(countries)
 
+    for (let i = 0; i < countries.length; i++) {
+      const sqlInsert = 'INSERT INTO country_info (country_name, country_code, calling_code, capital, region) VALUES (?, ?, ?, ?, ?);'
+      db.query(sqlInsert, [countries[i].name, countries[i].alpha3Code, countries[i].callingCodes[0], countries[i].capital, countries[i].region], (err, result) => {
+        console.log(err)
+      })
+    }
+  }
+  //saveCountriesToDB()
+})
+
+//Save data to eurbase_table table
+app.get('/api/rates', (req, res) => {
+  const saveRatesToDB = async () => {
+    let day = 1
+    for (day = 1; day <= 30; day++) {
+      let resp = []
+      if (day <= 9) {
+        resp = await getRatesfromFixer("0" + day)
+      } else {
+        resp = await getRatesfromFixer(day)
+      }
+      console.log(resp.data)
+      const result = resp.data
+      const sqlInsert = 'INSERT INTO eurbase_table (date, AUD, BRL, CNY, GBP, USD) VALUES (?, ?, ?, ?, ?, ?);'
+      db.query(sqlInsert, [result.date, result.rates.AUD, result.rates.BRL, result.rates.CNY, result.rates.GBP, result.rates.USD], (err, result) => {
+        console.log(err)
+      })
+    }
+  }
+  //saveRatesToDB()
+})
+
+//Select data from mysql COUNTRY_INFO table
+app.get('/countries', (req, res) => {
+  const sqlSelect = 'SELECT * FROM country_info;'
+  db.query(sqlSelect, (err, result) => {
+    res.send(result)
+    //console.log(result)
+  })
+})
+
+//Select data from mysql EURBASE_TABLE table
+app.get('/rates', (req, res) => {
+  const sqlSelect = 'SELECT * FROM eurbase_table;'
+  db.query(sqlSelect, (err, result) => {
+    res.send(result)
+  })
 })
 
 app.listen(port, () => {
